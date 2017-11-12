5932e08f4b24702f1748165b6cccb99d9aa167e8
baehyeonbin <baehynebin@hotmail.co.kr>
2017-11-12 23:38:40 +0900

    입력 폼  프론트 일부 작성

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var bodyParser = require('body-parser');
 var index = require('./routes/index');
 var users = require('./routes/users');
 var post = require('./routes/post');
+var aboutme = require('./routes/aboutme');
 
 var app = express();
 
@@ -26,6 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/users', users);
 app.use('/post', post);
+app.use('/aboutme', aboutme);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
