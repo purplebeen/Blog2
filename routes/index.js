6ddb43f4f65208a0baceaee22f266539cb257ae7
baehyeonbin <baehynebin@hotmail.co.kr>
2017-11-14 10:28:22 +0900

    포스트 리스트 조회 기능 추가

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
+var Post = require('../models/Post');
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index');
+  Post.find({}, (err, posts) => {
+    if(err) console.log(err.stack);
+    console.log(posts);
+    res.render('index', {
+        postList : posts
+      });
+  });
 });
+
 
 module.exports = router;
