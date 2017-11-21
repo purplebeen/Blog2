6a4bd8a17c31cd6ab9a8932ff4b401a3fc3917c8
baehyeonbin <baehynebin@hotmail.co.kr>
2017-11-21 12:23:25 +0900

    카테고리별 조회 기능 추가(백엔드)

--- routes/categories.js
+++ routes/categories.js
@@ -1,12 +1,23 @@
 var express = require('express');
 var router = express.Router();
 var Category = require('../models/Category');
+var Post = require('../models/Post');
 
 router.post('/add', (req, res) => {
     console.log(req.body);
     Category.create(req.body, (err,category) => {
         if(err) throw err;
         res.send('<script>alert("성공적으로 생성되었습니다!");location.href="/posts/write"</script>');
+    });
+});
+
+router.get('/:categoryName', (req, res) => {
+    Post.findAll({category : req.params.categoryName}, (err, posts) => {
+        if(err) console.log(err);
+        res.render('index', {
+            postList : posts, 
+            user : req.user
+        });
     });
 });
 
