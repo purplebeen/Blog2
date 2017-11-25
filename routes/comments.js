e2080c6a11181dc11a604f1284c5e12228e8047d
baehyeonbin <baehynebin@hotmail.co.kr>
2017-11-25 21:13:56 +0900

    댓글 추가 / 삭제 문제 해결

--- routes/comments.js
+++ routes/comments.js
@@ -3,26 +3,26 @@ var router = express.Router();
 
 var Post = require('../models/Post');
 
-router.post('/write/:postTitle', (req, res) => {
-    Post.findOne({title : req.params.postTitle},(err, post) => {
+router.post('/write/:id', (req, res) => {
+    Post.findOne({_id : req.params.id},(err, post) => {
         if(err) throw err
         var comment = req.body;
         console.log(comment);
         post.comments.push(comment);
         post.save((err) => {
             if(err) throw err;
-            res.redirect('/posts/view/'+req.params.postTitle);            
+            res.redirect('/posts/view/'+req.params.id);            
         });
     });
 });
 
-router.get('/delete/:postTitle/:commentId', (req, res) => {
-    Post.findOne({title : req.params.postTitle}, (err, post) => {
+router.get('/delete/:postId/:commentId', (req, res) => {
+    Post.findOne({_id : req.params.postId}, (err, post) => {
         if(err) throw err;
         post.comments.pull({_id : req.params.commentId});
         post.save((err) => {
             if(err) throw err;
-            res.redirect('/posts/view/' + req.params.postTitle);
+            res.redirect('/posts/view/' + req.params.postId);
         });
     });
 });
